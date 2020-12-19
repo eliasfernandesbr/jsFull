ba3df8e483209a4e5580201836d6bf13408c9890
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2020-12-19 00:32:07 -0300

    firstCommit

---
+++ switch.js
@@ -0,0 +1,27 @@
+// var idade = 19;
+
+
+// if (idade < 18 || idade > 70){
+//     console.log("pode");
+//     console.log("Qual é o seu pedido");
+// }
+
+// else{
+//     console.log("Não pode");
+//     console.log("Volte futuramente");
+// }
+
+var nota1 = 9.0;
+var nota2 = 5.8;
+
+var media = (nota1 + nota2) / 2;
+var conceito ="".
+
+if(media >=8){
+conceito = "Ótimo!"
+}else if (media > 6.5){
+
+}else{
+
+
+}
