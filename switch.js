7c4c0b106791059fc83e2714fa8442729303db70
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2020-12-19 00:53:15 -0300

    secondCommit

--- switch.js
+++ switch.js
@@ -1,6 +1,5 @@
 // var idade = 19;
 
-
 // if (idade < 18 || idade > 70){
 //     console.log("pode");
 //     console.log("Qual é o seu pedido");
@@ -11,17 +10,37 @@
 //     console.log("Volte futuramente");
 // }
 
-var nota1 = 9.0;
-var nota2 = 5.8;
+var nota1 = 0.0;
+var nota2 = 0.0;
 
 var media = (nota1 + nota2) / 2;
-var conceito ="".
+var conceito = "";
 
-if(media >=8){
-conceito = "Ótimo!"
-}else if (media > 6.5){
+if (media >= 8) {
+  conceito = "Ótimo!";
+} else if (media > 6.5) {
+  conceito = "Bom";
+} else {
+  conceito = "Regular";
+}
 
-}else{
+console.log(media);
+console.log(conceito);
 
+switch (conceito) {
+  case "Ótimo!":
+    console.log("PARABÉNS, VOCÊ É UM ÓTIMO ALUNO");
+    break;
 
+  case "Bom":
+    console.log("VOCÊ ESTÁ INDO MUITO BEM");
+    break;
+
+  case "Regular":
+    console.log("VOCÊ PODE ESTUDAR MAIS");
+    break;
+
+  default:
+    console.log("HOUVE ALGUM ERRO");
+    break;
 }
