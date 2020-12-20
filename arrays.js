92b31210f2b3cf32b23f7d0099af87bb225487e4
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2020-12-19 23:12:10 -0300

    finalDayCommit

--- arrays.js
+++ arrays.js
@@ -1,2 +1,7 @@
-var vetor = new Array();
-console.log(vetor);
+
+var alunos = new Array("Elias", "Dante", "Jhessiny", "Bidu", "Poly");
+console.log(alunos[4]);
+
+for (var i = 0; i < alunos.length; i++){
+    console.log(alunos[i]);
+}
---
+++ funcoes.js
@@ -0,0 +1,15 @@
+// var n1 = 10;
+// var n2 = 20;
+
+// function soma(n1, n2) {
+//   return ((n1+n2)/2);
+// }
+// console.log(soma(10,20));
+
+function saudacao() {
+  return "Olá mundo!";
+}
+
+var s = saudacao();
+
+console.log(s);
