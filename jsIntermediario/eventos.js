7020b377bbafa3a93b48c4c24a7e977650ef625d
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2021-01-03 19:15:33 -0300

    Criado botão hideH1, onde ao clicar esconde o texto do H1

--- jsIntermediario/eventos.js
+++ jsIntermediario/eventos.js
@@ -3,6 +3,12 @@ function h1click() {
 }
 
 function changeH1() {
+  let input = document.getElementById("texto");
   let h1 = document.querySelector("h1");
-  console.log(h1);
+  h1.innerText = input.value;
+}
+
+function hideH1(){
+    let h1 = document.getElementById("testeH1")
+    h1.style.display = "none";
 }
