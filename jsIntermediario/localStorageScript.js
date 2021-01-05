f549ecc9e82a44fabc0198ea6a8430aede257a6e
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2021-01-04 23:04:59 -0300

    Pagina sorteio

--- jsIntermediario/localStorageScript.js
+++ jsIntermediario/localStorageScript.js
@@ -4,17 +4,28 @@
 // console.log(n);
 
 
-onload = function(){
-    let nome = localStorage.getItem
+// onload = function(){
+//     let nome = localStorage.getItem("nome");
+//     let h1 = document.getElementById("nome");
+//     h1.innerHTML = nome;
+// }
+
+
+// function atualizar(element){
+//     let valor = element.value;
+//     console.log(valor);
+
+//     let h1 = document.getElementById("nome");
+//     h1.innerHTML = valor;
+
+//     localStorage.setItem("nome", valor)
+// }
+let a = {
+    nome: "Dante",
+    n1: 7.3
 }
 
+localStorage.setItem("aluno", JSON.stringify(a));
 
-function atualizar(element){
-    let valor = element.value;
-    console.log(valor);
-
-    let h1 = document.getElementById("nome");
-    h1.innerHTML = valor;
-
-    localStorage.setItem("nome", valor)
-}
+// let n = localStorage.getItem("numero");
+// console.log(n);
